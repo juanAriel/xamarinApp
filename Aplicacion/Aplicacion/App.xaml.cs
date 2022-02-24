e80d6fecf7fb74b98f0a2a14eecbdedbb78df5fa
juanAriel <arielvia2013@gmail.com>
2022-02-23 22:50:47 -0400

    primer commit o inicio del proyecto

---
+++ Aplicacion/Aplicacion/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplicacion
+{
+    public partial class App : Application
+    {
+        //son los estados que la aplicacion va a tener mantener informacion o procesos vivos o en ejecucion de segundo plano
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
